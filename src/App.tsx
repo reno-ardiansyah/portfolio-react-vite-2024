db05776764730022973273a0f741c25952f994b8
Reno Ardiansyah🔥 <reno75874@gmail.com>
2024-11-30 19:44:44 +0700

    setup react route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "@/pages/HomePage";
+import AboutPage from "@/pages/AboutPage";
+import GalleryPage from "@/pages/GalleryPage";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/about" element={<AboutPage/>} />
+        <Route path="/gallery" element={<GalleryPage/>} />
+      </Routes>
+    </Router>
   )
 }
---
+++ src/pages/GalleryPage.tsx
@@ -0,0 +1,9 @@
+const GalleryPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Gallery Page</h1>
+    </div>
+  );
+};
+
+export default GalleryPage;
